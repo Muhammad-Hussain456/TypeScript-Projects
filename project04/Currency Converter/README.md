19696ef22ca171f9e130c1065494128e29abab83
Muhammad-Hussain456 <muhammadhucen456@gmail.com>
2024-09-10 23:51:02 +0500

    updated

---
+++ project04/Currency Converter/README.md
@@ -0,0 +1,23 @@
+Governor Sindh Kamran Khan Initiative For Cloud Applied Generative AI.                                    
+Fifth Project - Project04                                                                                 
+- Currency Converter:                                                                                     
+A simple Currency Converter using Typescript, Node.js, Inquirer, Chalk and Chalk-animation.               
+
+-- Installation:
+  1. Clone the Repository
+    https://github.com/MuhammadHussain122/Projects/tree/master/Project04
+  2. Install package
+    npx mh05_currency_converter
+
+-- features: 
+ - Ask user to select a currency to convert from.                                                         
+ - Ask user to select a currency to convert to.                                                           
+ - Perfrom currency conversion according to the user's selection.                                         
+
+ 
+ 
+-- License:
+This project is licensed under the MIT Lisensce. 
+
+-- Contact:
+For any question or feedback, feel free to reach out to me at muhammadhucen830@gmail.com.
---
+++ project04/Currency Converter/index.js
@@ -0,0 +1,88 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+const currency = {
+    USD: 1,
+    PKR: 277.90,
+    EURO: 0.92,
+    YUAN: 7.23,
+    INR: 83.2,
+    YER: 249.97,
+    SAR: 3.75,
+    IQD: 1309.0,
+    IRR: 42000.0,
+};
+const rest = () => {
+    return new Promise((res) => {
+        setTimeout(res, 2000);
+    });
+};
+async function welcome() {
+    let title = chalkAnimation.neon("currency converter");
+    await rest();
+    title.stop();
+}
+await welcome();
+async function currencyConverter() {
+    let userInputs = await inquirer.prompt([
+        {
+            name: "currencyFrom",
+            message: "from:",
+            type: "list",
+            choices: [
+                "USD",
+                "PKR",
+                "EURO",
+                "YUAN",
+                "INR",
+                "YER",
+                "SAR",
+                "IQD",
+                "IRR",
+            ],
+        },
+        {
+            name: "currencyTo",
+            message: "to:",
+            type: "list",
+            choices: [
+                "USD",
+                "PKR",
+                "EURO",
+                "YUAN",
+                "INR",
+                "YER",
+                "SAR",
+                "IQD",
+                "IRR",
+            ],
+        },
+        {
+            name: "amount",
+            message: "Enter your amount:",
+            type: "number",
+        },
+    ]);
+    let fromCurrency = currency[userInputs.currencyFrom];
+    let toCurrency = currency[userInputs.currencyTo];
+    let amount = userInputs.amount;
+    let baseAmount = amount / fromCurrency;
+    let convertedAmount = baseAmount * toCurrency;
+    console.log(chalk.green(convertedAmount.toFixed(2)));
+}
+async function useCurrencyConverterAgain() {
+    do {
+        await currencyConverter();
+        var getUserInputAgain = await inquirer.prompt({
+            name: "getInputAgain",
+            message: "Do you want to use currency converter again?",
+            type: "input",
+        });
+    } while (getUserInputAgain.getInputAgain === "y" ||
+        getUserInputAgain.getInputAgain === "yes" ||
+        getUserInputAgain.getInputAgain === "Y" ||
+        getUserInputAgain.getInputAgain === "Yes" ||
+        getUserInputAgain.getInputAgain === "YES");
+}
+useCurrencyConverterAgain();
